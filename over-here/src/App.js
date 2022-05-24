ddd6d7c60d08638e1f007148ba14df6205ad39c7
rvcoding93 <99891357+rvcoding93@users.noreply.github.com>
2022-05-24 13:07:18 -0400

    added app.js functionality, fixed breaking bugs

--- over-here/src/App.js
+++ over-here/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import About from "./components/about";
+import LandingPage from "./components/landing-page";
+import Home from "./components/home";
+import UploadPage from "./components/upload-page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/:path(|about)">
+          <About />
+        </Route>
+        <Route path="/landing-page">
+          <LandingPage
+            overlapGroup="/img/ukraine-by-theno7er-df0matk-250t-1@1x.png"
+            flag_Of_Ukraine1="/img/flag-of-ukraine-1@2x.png"
+            title="Enter Here"
+          />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/upload-page">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
