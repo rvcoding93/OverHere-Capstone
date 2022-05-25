0203c8c3cf89edb78bddd91d4be1c25f4e23b06f
rvcoding93 <99891357+rvcoding93@users.noreply.github.com>
2022-05-25 13:47:05 -0400

    reconvigured CSS to try and fix the render bug

--- over-here/src/components/about.js
+++ over-here/src/components/about.js
@@ -7,21 +7,21 @@ function About () {
         render (
         <body> 
         {/* style="margin: 0; background: #ffffff" --- this styling came from the BODY tab*/}
-    <input type="hidden" id="anPageName" name="page" value="about" />
-    <div class="container-center-horizontal">
-      <div class="about screen">
-        <div class="overlap-group">
-          <div class="rectangle-40"></div>
-          <div class="flex-row">
-            <div class="rectangle-45"></div>
-            <div class="rectangle-48"></div>
-          </div>
-          <div class="rectangle-46"></div>
-          <div class="rectangle-47"></div>
+        <input type="hidden" id="anPageName" name="page" value="about" />
+        <div class="container-center-horizontal">
+        <div class="about screen">
+            <div class="overlap-group">
+            <div class="rectangle-40"></div>
+            <div class="flex-row">
+                <div class="rectangle-45"></div>
+                <div class="rectangle-48"></div>
+            </div>
+            <div class="rectangle-46"></div>
+            <div class="rectangle-47"></div>
+            </div>
         </div>
-      </div>
-    </div>
-  </body>
+        </div>
+    </body>
         )
     );
   }
