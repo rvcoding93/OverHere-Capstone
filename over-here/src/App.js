74cf15db1bca86a127bf308a21fdc16f52d82f11
rvcoding93 <99891357+rvcoding93@users.noreply.github.com>
2022-05-26 14:21:07 -0400

    cleaned up App.js folder, redefined routes and condensed code. Still not rendering on browser

--- over-here/src/App.js
+++ over-here/src/App.js
@@ -1,30 +1,29 @@
 import "./App.css";
-import { Route, Routes, Router } from "react-router-dom";
+import { Route, Routes, Router,Link } from "react-router-dom";
 import React from "react";
 import About from "./components/about";
 import LandingPage from "./components/landing-page";
 import Home from "./components/home";
 import UploadPage from "./components/upload-page";
+import ErrorPage from "./components/ErrorPage"
+
 
 
 
 function App() {
   return (
     <Router>
+      <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/upload-page">Contact</Link>
+      </nav>
       <Routes>
-        <Route path="/:path(|about)">
-          <About />
-        </Route>
-        <Route path="/landing-page">
-          <LandingPage
-          />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/upload-page">
-          <UploadPage />
-        </Route>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/upload-page" element={<UploadPage/>}/>
+        <Route path="*" element={<ErrorPage/>} />
       </Routes>
     </Router>
   );
