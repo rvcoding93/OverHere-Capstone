f68961ed10575e54c427b7cdbfcb76c3c187fce5
rvcoding93 <99891357+rvcoding93@users.noreply.github.com>
2022-05-26 20:20:10 -0400

    pushing up latest work before Backend call

---
+++ over-here/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import { Route, Routes, Router,Link } from "react-router-dom";
+import React from "react";
+import About from "./components/about";
+import LandingPage from "./components/landing-page";
+import Home from "./components/home";
+import UploadPage from "./components/upload-page";
+import ErrorPage from "./components/ErrorPage"
+
+
+
+
+function App() {
+  return (
+    <Router>
+      <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/upload-page">Contact</Lr3ink>
+      </nav>
+      <Routes>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/upload-page" element={<UploadPage/>}/>
+        <Route path="*" element={<ErrorPage/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
